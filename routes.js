404ec6141954c77e001495db1e6bc008c657f236
amayones <mayonedrive1@gmail.com>
2025-08-01 10:21:48 +0700

    feat: Query Parameter

--- routes.js
+++ routes.js
@@ -22,6 +22,21 @@ const routes = [
             return `about`;
         }
     },
+    {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+
+            const { name = "stranger" } = request.params;
+            const { lang } = request.query;
+
+            if (lang === 'id') {
+                return `Hai, ${name}!`;
+            }
+
+            return `Hello, ${name}!`;
+        },
+    },
 ]
 
 module.exports = routes;
